8665870a6f8221b9b43b10923cc174f71236c527
samuellangajr <samuellangajr@gmail.com>
2024-10-18 09:16:48 +0200

    final 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
 "use client";
 import { useState } from "react";
-import Image from "next/image";
-import background from "../../public/images/background.png";
 
 export default function PredictPage() {
   const [formData, setFormData] = useState({
